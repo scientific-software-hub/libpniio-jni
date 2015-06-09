c74d014f5f329101b88a92ef111eef1d97040d2c
Ingvord <mail@ingvord.ru>
2015-06-09 10:55:50 +0300

    progress

--- java/src/test/java/hzg/wpn/nexus/libpniio/jni/LibpniioJniTest.java
+++ java/src/test/java/hzg/wpn/nexus/libpniio/jni/LibpniioJniTest.java
@@ -1,6 +1,36 @@
 package hzg.wpn.nexus.libpniio.jni;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 public class LibpniioJniTest {
+
+    @SuppressWarnings("restriction")
+    private static Unsafe getUnsafe() {
+        try {
+
+            Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+            singleoneInstanceField.setAccessible(true);
+            return (Unsafe) singleoneInstanceField.get(null);
+
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Address size (B):" + getUnsafe().addressSize());
+    }
 
     @org.junit.Test
     public void testCreateFile() {
