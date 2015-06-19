e1970fbfdcc2776d7dfbfa4a29d3137c9322943f
Igor Khohkraikov <mail@ingvord.ru>
2015-06-19 12:00:24 +0200

    extract native lib into xenv/lib

--- jni_cpp/test/main.cpp
+++ jni_cpp/test/main.cpp
@@ -13,16 +13,16 @@ using namespace pni::io::nx;
 using namespace pni::io::nx::h5;
 
 int main(int argc,char **argv) {
-    h5::nxfile f = h5::nxfile::open_file("/home/khokhria/Projects/hzg.wpn/libpniio-jni/target/test.h5");
+    h5::nxfile f = h5::nxfile::open_file("/home/khokhria/Projects/hzg.wpn/jDataFormatServer/var/test.h5");
 
-    h5::nxfield o = get_object(f.root(), nxpath::from_string("/entry/scan/data/image/value"));
+    h5::nxfield o = get_object(f.root(), nxpath::from_string("/entry/data/data"));
 
     std::cout << "field.name = " << o.name() << std::endl;
     std::cout << "field.size = " << o.size() << std::endl;
 
-    cimg_library::CImg<int32_t> img(512,512);
+    cimg_library::CImg<int16_t> img(512,512);
 
-    o(1,pni::core::slice(0,512),pni::core::slice(0,512)).read(262144,img.data());
+    o(0,pni::core::slice(0,512),pni::core::slice(0,512)).read(262144,img.data());
 
     img.save_jpeg("test.out.jpeg");
 
